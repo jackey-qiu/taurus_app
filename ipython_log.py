7ff5bad4722901576b79f9c3288244ba9dba2263
jackey-qiu <crqiu2@gmail.com>
2023-02-21 11:04:24 +0100

    after merging

--- ipython_log.py
+++ ipython_log.py
@@ -1,8 +1,2 @@
 # IPython log file
 
-# Thu, 16 Feb 2023 14:45:16
-wm mot01# Thu, 16 Feb 2023 14:45:22
-wa# Thu, 16 Feb 2023 14:45:29
-wm eh_mot01# Thu, 16 Feb 2023 14:47:09
-ascan exp_dmy01 1 20 20 0.2# Thu, 16 Feb 2023 14:47:36
-ascan exp_dmy01 1 20 20 2
